e153881e235f717010e8897ae1dc5483d0ff035f
Rayeesa Mahmood <151394585+RayeesaMahmood@users.noreply.github.com>
2026-01-27 17:45:53 +0530

    Update voice_of_the_patient.py

--- voice_of_the_patient.py
+++ voice_of_the_patient.py
@@ -1,9 +1,4 @@
-# if you dont use pipenv uncomment the following:
-# from dotenv import load_dotenv
-# load_dotenv()
 
-#Step1: Setup Audio recorder (ffmpeg & portaudio)
-# ffmpeg, portaudio, pyaudio
 import logging
 import speech_recognition as sr 
 from pydub import AudioSegment
