0b48ee164bfd4e02c0376ef43d3e342e01fa951e
Rayeesa Mahmood <151394585+RayeesaMahmood@users.noreply.github.com>
2026-01-27 17:44:33 +0530

    Update brain_of_the_doctor.py

--- brain_of_the_doctor.py
+++ brain_of_the_doctor.py
@@ -1,6 +1,4 @@
-# if you dont use pipenv uncomment the following:
-# from dotenv import load_dotenv
-# load_dotenv()
+
 #Step1: Setup GROQ API key
 import os
 
@@ -19,10 +17,7 @@ def encode_image(image_path):
 from groq import Groq
 
 query="Is there something wrong with my face?"
-#model = "meta-llama/llama-4-maverick-17b-128e-instruct"
 model="meta-llama/llama-4-scout-17b-16e-instruct"
-#model = "meta-llama/llama-4-scout-17b-16e-instruct"
-#model="llama-3.2-90b-vision-preview" #Deprecated
 
 def analyze_image_with_query(query, model, encoded_image):
     client=Groq()  
